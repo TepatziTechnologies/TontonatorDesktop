9eba5922930d94a3d5e9343fff83f85e08653831
M RODRIGO <M RODRIGO@DESKTOP-8403L8N>
2022-12-24 16:46:21 -0600

    Agregar archivos de proyecto.

---
+++ Models/Enums/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TontonatorDesktopApp.Models.Enums
+{
+	public enum UserRole
+	{
+		Administrator,
+		Moderator,
+		Editor,
+		User
+	}
+}
---
+++ Models/UserApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TontonatorDesktopApp.Models
+{
+	class UserApp
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+
+		public UserApp()
+		{
+
+		}
+	}
+}
